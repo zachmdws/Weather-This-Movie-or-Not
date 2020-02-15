b21c6c585384bc4e7f65f4ba6e25b4391f12db81
Zach M <zatchmdws@gmail.com>
2020-02-15 13:35:28 -0500

    Made function to grab location and weather of user

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,3 +9,127 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getLocation();
+
+function getLocation() { 
+
+    if(navigator.geolocation) { 
+        
+        navigator.geolocation.getCurrentPosition(getWeather);
+
+    }
+}
+    function getWeather(position) { 
+
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        var APIkey = "4971bbf933e132e86212d1bc1c100553";
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+ APIkey;
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){ 
+            var weather = (response.weather[0].main);
+            var icon = (response.weather[0].icon);
+
+            if(response.weather[0].icon) { 
+                icon = ("<img id='weatherIcon' src='http://openweathermap.org/img/wn/"+ icon + "@2x.png");
+            }
+
+            console.log(response);
+        })
+
+    }
+
+
